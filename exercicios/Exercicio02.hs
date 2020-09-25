7364d09cb2100cad9a87d04bf13ba80aedb73d2d
Matheus Mazoni <mmazoni.andrade@gmail.com>
2020-09-25 00:22:36 +0000

    exercicios capítulo 2

--- exercicios/Exercicio02.hs
+++ exercicios/Exercicio02.hs
@@ -16,4 +16,36 @@ ex21c = ["A" ++ [x] ++ "BB" | x<-['a'..'h']]
 ---ex 2.1.e
 
 ex21e :: [Double]
-ex21e = [x | x/2]
+ex21e = [1/2^x | x<-[0..5]]
+
+---ex 2.1.f
+
+ex21f :: [Int]
+ex21f = [1 + x*9 | x<-[0..7]]
+
+---ex 2.1.h
+
+ex21h :: [Char]
+ex21h = [ x | x<-['@'..'L']]
+
+-----------------------------
+
+---ex 2.2
+
+verificaPar :: String -> Bool
+verificaPar x = even $ length x
+
+---ex 2.3
+
+listaAvessa :: [String] -> [String]
+listaAvessa x = [ reverse xs | xs <- x]
+
+---ex 2.4
+
+tamanhoSemPar :: [String] -> [Int]
+tamanhoSemPar x = [length xs | xs <- x, mod (length xs) 2 /= 0]
+
+---ex 2.5
+
+cabeca :: [Char] -> Char
+cabeca x = last $ reverse x
